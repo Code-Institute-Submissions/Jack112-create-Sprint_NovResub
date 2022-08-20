c5905456667c18c2fabaa10b37a5b6802a3bef47
Jack Conroy <jackconroy777@gmail.com>
2022-08-20 08:36:52 +0000

    Setup projects app

---
+++ projects/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Project(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
--- sprint/urls.py
+++ sprint/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('projects/', include('projects.urls')),
     path('accounts/',  include('allauth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
