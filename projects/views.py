3d984c842389b1d5e2a18edf259076e3b17e5f7d
Jack Conroy <jackconroy777@gmail.com>
2022-08-20 18:06:40 +0000

    Setup projects template, project view and media url configuration in urls.py

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+    }
+
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, project_id):
+
+    context = {}
+    return render(request, 'projects/project.html', context)
