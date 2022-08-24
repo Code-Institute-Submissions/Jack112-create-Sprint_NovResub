8c62fd65aa4bd72511dca28c1418d742491c2b48
Jack Conroy <jackconroy777@gmail.com>
2022-08-24 20:38:54 +0000

    Add remove item view and url

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 
-
 def view_bag(request):
     """
     A view to render the bag template
@@ -28,3 +27,17 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def remove_item(request, item_id):
+    """
+    A view to remove an item from the bag
+    """
+
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        del bag[item_id]
+
+    request.session['bag'] = bag
+    return render(request, 'bag/bag.html')
