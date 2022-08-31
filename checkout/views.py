03e6b9cd85359431c301201804bb3ae13d4a01a8
Jack Conroy <jackconroy777@gmail.com>
2022-08-31 20:48:41 +0000

    Test stripe integration and add form handler logic

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,9 @@ def checkout(request):
     A view to render the checkout page
     """
 
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
 
     if not bag:
@@ -20,13 +23,21 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
 
     order_form = OrderForm()
 
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LO3O5DygYmPeIrevJYyv2d0reTyVqZAttBFpoyhWm5NKAS8Wa9BTchtwwOvyQM2dYZa8Na6HGxhfYUf2h1ujdjd00i2NtMVaj',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
