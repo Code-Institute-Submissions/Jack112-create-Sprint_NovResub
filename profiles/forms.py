9d1d297e3541703e91fcd04d66fc9f56ea2a90f0
Jack Conroy <jackconroy777@gmail.com>
2022-09-08 21:10:27 +0000

    Update user profile form and profile template

---
+++ profiles/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add custom labels to user profile form
+        """
+        super().__init__(*args, **kwargs)
+        labels = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+            'default_country': 'Country',
+        }
+    
+        for field in self.fields:
+            if self.fields[field].required:
+                self.fields[field].label = f'{labels[field]} *'
+            else:
+                self.fields[field].label = labels[field]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from checkout.models import Order
+from .forms import UserProfileForm
 
 def profile(request):
     """
     A view to render the users profile
     """
-    return render(request, 'profiles/profile.html')
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    orders = profile.orders.all()
+    form = UserProfileForm(instance=profile)
+
+    context = {
+        'profile': profile,
+        'orders': orders,
+        'form': form,
+    }
+
+    return render(request, 'profiles/profile.html', context)
 
