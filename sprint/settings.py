6b8747ffb7fbd5b7ec31b08b8dcdd2e6399402d3
Jack Conroy <jackconroy777@gmail.com>
2022-09-14 20:08:08 +0000

    Update database conditional

--- sprint/settings.py
+++ sprint/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'sprint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if env('DATABASE_URL'):
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(env('DATABASE_URL'))
     }
