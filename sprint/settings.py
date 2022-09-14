5dd8e45b646c77ab61a26d924ff6bd09843c5063
Jack Conroy <jackconroy777@gmail.com>
2022-09-14 19:47:01 +0000

    Setup database_url environment variable and connect app to heroku

--- sprint/settings.py
+++ sprint/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-_ln2lkmoj+9t1sl-tk7(ej^vdt!q*g-1%=1tzsb6a8iip)iw0d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sprint-designs.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'sprint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if env('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(env('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
