02b024c788b17a8235ee8f49943fbe4be6c4c60f
Jack Conroy <jackconroy777@gmail.com>
2022-09-15 12:18:26 +0000

    Remove development key logic from settings.py

--- sprint/settings.py
+++ sprint/settings.py
@@ -29,9 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-print(env('DEVELOPMENT'))
 
 ALLOWED_HOSTS = ['sprint-designs.herokuapp.com', 'localhost']
 
