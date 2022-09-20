eae99b1633207969b0e0c10a12267604c10ededa
Jack Conroy <jackconroy777@gmail.com>
2022-09-20 20:06:02 +0000

    Update stripe_element.js file to fix jshint warnings

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -40,7 +40,7 @@ card.addEventListener('change', function(e) {
             <i class="fas fa-times"></i>
         </span>
         <span>${e.error.message}</span>
-        `
+        `;
 
         $(errorDiv).html(html);
     } else {
@@ -117,5 +117,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
