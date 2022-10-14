75f476cca5f9cb507642bd6aeacc2b80b4c9c3bb
Jack Conroy <jackconroy777@gmail.com>
2022-10-14 17:54:02 +0000

    Update contact view to fix pep8 errors

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def contact(request):
+    """
+    A view to render the contact page.
+    """
     return render(request, 'contact/contact.html')
