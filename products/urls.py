6a6f933d5735e4819633a6cf5ffa97c4fa2767ed
Jack Conroy <jackconroy777@gmail.com>
2022-10-14 18:03:04 +0000

    Update products urls and views with review model

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,27 @@ from . import views
 urlpatterns = [
     path('templates/', views.view_templates, name='templates'),
     path('designs/', views.view_designs, name='designs'),
-    path('templates/<str:product_id>/', views.product_detail, name='template_product_detail'),
-    path('designs/<str:product_id>/', views.product_detail, name='design_product_detail'),
+    path('review/add/<str:product_id>/', views.add_review, name='add_review'),
+    path(
+        'review/delete/<str:review_id>/', 
+        views.delete_review, 
+        name='delete_review'
+        ),
+    path(
+        'templates/<str:product_id>/',
+        views.product_detail,
+        name='template_product_detail'
+        ),
+    path(
+        'designs/<str:product_id>/',
+        views.product_detail,
+        name='design_product_detail'
+        ),
     path('add/', views.add_product, name='add_product'),
     path('edit/<str:product_id>/', views.edit_product, name='edit_product'),
-    path('delete/<str:product_id>/', views.delete_product, name='delete_product'),
+    path(
+        'delete/<str:product_id>/',
+        views.delete_product,
+        name='delete_product'
+    ),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Review
 from .forms import ProductForm
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse, get_object_or_404
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 def view_templates(request):
     """
-    Display all products
+    A view to display all template products
     """
 
     products = Product.objects.filter(product_type="T")
@@ -34,14 +34,65 @@ def view_designs(request):
     return render(request, 'products/designs.html', context)
 
 
+@login_required
+def add_review(request, product_id):
+    """ A view to add a product review """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        rating = request.POST.get('rating', 1)
+        content = request.POST.get('content', '')
+        user = request.user
+        review = Review.objects.create(
+            product=product,
+            rating=rating,
+            content=content,
+            created_by=user
+        )
+
+        if product.product_type == 'T':
+            messages.success(request, 'Review successfully added!')
+            return redirect(reverse(
+                'template_product_detail', args=[product.id]))
+        elif product.product_type == 'D':
+            messages.success(request, 'Review successfully added!')
+            return redirect(reverse(
+                'design_product_detail', args=[product.id]))
+
+
+@login_required
+def delete_review(request, review_id):
+    """ A view to delete a product review """
+    review = get_object_or_404(Review, pk=review_id)
+    user = request.user
+    product = review.product
+
+    # Admin user can delete all reviews.
+    # Regular user can only review created by them.
+    if user == review.created_by or user.is_superuser:
+        review.delete()
+        messages.success(request, 'Review successfully deleted!')
+        return redirect(reverse(
+                    'template_product_detail', args=[product.id]))
+    else:
+        if product.product_type == 'T':
+            messages.error(request, 'You cannot delete this review!')
+            return redirect(reverse(
+                    'template_product_detail', args=[product.id]))
+        elif product.product_type == 'D':
+            messages.error(request, 'You cannot delete this review!')
+            return redirect(reverse(
+                    'design_product_detail', args=[product.id]))
+
+
 def product_detail(request, product_id):
     """
     A view to display a single product
     """
 
     product = get_object_or_404(Product, pk=product_id)
-
-    print(product.product_type)
+    reviews = Review.objects.filter(product=product)
 
     if product.product_type == 'T':
         title = 'Templates'
@@ -51,6 +102,7 @@ def product_detail(request, product_id):
     context = {
         'product': product,
         'title': title,
+        'reviews': reviews,
     }
 
     return render(request, 'products/product_detail.html', context)
@@ -69,14 +121,18 @@ def add_product(request):
             product = form.save()
             messages.success(request, 'Successfully added product!')
             if product.product_type == 'T':
-                return redirect(reverse('template_product_detail', args=[product.id]))
+                return redirect(reverse(
+                    'template_product_detail', args=[product.id]))
             elif product.product_type == 'D':
-                return redirect(reverse('design_product_detail', args=[product.id]))
+                return redirect(reverse(
+                    'design_product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -100,15 +156,23 @@ def edit_product(request, product_id):
             product = form.save()
             messages.success(request, 'Successfully updated a product!')
             if product.product_type == 'T':
-                return redirect(reverse('template_product_detail', args=[product.id]))
+                return redirect(reverse(
+                    'template_product_detail',
+                    args=[product.id]))
             elif product.product_type == 'D':
-                return redirect(reverse('design_product_detail', args=[product.id]))
+                return redirect(reverse(
+                    'design_product_detail',
+                    rgs=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
-        messages.info(request, f'You are editing the following product: {product.name}')
-        
+        messages.info(
+            request,
+            f'You are editing the following product: {product.name}')
+
     template = 'products/edit_product.html'
     context = {
         'form': form,
@@ -120,11 +184,11 @@ def edit_product(request, product_id):
 
 @login_required
 def delete_product(request, product_id):
-    """ delete a store product """
+    """ A view to delete a store product """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
-    
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
 
