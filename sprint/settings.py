dee05a9b37db29d9bbba22d10cbb9090a430d066
Jack Conroy <jackconroy777@gmail.com>
2022-10-25 18:56:11 +0000

    Set debug back to false

--- sprint/settings.py
+++ sprint/settings.py
@@ -29,9 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['sprint-designs.herokuapp.com', 'localhost']
