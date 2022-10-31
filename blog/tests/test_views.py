d050569f5fd45a05139fe72f1bafbd61217f3bcd
Jack Conroy <jackconroy777@gmail.com>
2022-10-31 13:55:57 +0000

    Add CRUD testing to blog tests

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -16,6 +16,24 @@ class TestViews(TestCase):
         blog = Blog.objects.create(title="Test Blog", name="Jack")
         self.blogs = Blog.objects.all()
 
+        # User setup and login
+        username = 'Test'
+        email = 'test@test.com'
+        password = 'testpassword1234'
+        first_name = 'testUserFirst'
+        last_name = 'testUserLast'
+        user_model = get_user_model()
+        self.user = user_model.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name)
+
+        # Login test user
+        login = self.client.login(email=email, password=password)
+        self.assertTrue(login)
+
     def test_blogs_GET(self):
         '''
         Test that blog view uses blogs.html template
@@ -36,3 +54,60 @@ class TestViews(TestCase):
             follow=True)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/single_blog.html')
+
+    def test_add_blog_GET(self):
+        '''
+        Test that add_blog view uses add_blog.html template
+        '''
+        response = self.client.get(reverse('add_blog'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/add_blog.html')
+
+    def test_add_blog_view_POST(self):
+        '''
+        Test that post requests to add_blog view create a new blog instance
+        '''
+        blog = {
+            'title': 'Test title',
+            'name': 'Test name',
+            'content': 'Test content',
+        }
+        response = self.client.post(reverse('add_blog'), blog)
+        self.assertEquals(response.status_code, 302)
+        self.assertEqual(Blog.objects.count(), 2)
+        self.assertEquals(self.blogs[1].title, blog['title'])
+
+    def test_edit_blog_GET(self):
+        '''
+        Test that edit_blog view uses edit_blog.html template
+        '''
+        response = self.client.get(reverse(
+            'edit_blog',
+            args=[self.blogs[0].id]))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/edit_blog.html')
+
+    def test_edit_blog_POST(self):
+        '''
+        Test that post requests to edit_blog view updates blog instance
+        '''
+        blog = self.blogs.get(title='Test Blog')
+        response = self.client.post(reverse(
+            'edit_blog',
+            kwargs={'blog_id': blog.id}),
+            {'title': 'Updated blog title'},
+            )
+        self.assertEqual(response.status_code, 200)
+        blog.refresh_from_db()
+        self.assertEqual(blog.title, 'Updated blog title')
+
+    def test_delete_blog(self):
+        '''
+        Test that delete_blog view deletes a blog instance
+        '''
+        response = self.client.get(reverse(
+            'delete_blog',
+            args=[self.blogs[0].id]
+        ))
+        self.assertEqual(Blog.objects.count(), 0)
+        self.assertEquals(response.status_code, 302)
